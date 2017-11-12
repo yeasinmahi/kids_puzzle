d423fa9f78b9bb222e35a1b6f40235279b29d7ad
yeasinmahi <yeasinmahi72@yahoo.com>
2017-11-12 19:46:59 +0600

    drag a object

---
+++ Assets/Script/drag.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drag : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objectPosition;
+		
+	}
+}
