14ca60b61a647e8ba5844904069cafe3854f840c
yeasinmahi <yeasinmahi72@yahoo.com>
2017-11-13 13:22:53 +0600

    drag and drop in one position

--- Assets/Script/drag.cs
+++ Assets/Script/drag.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class drag : MonoBehaviour {
 
-	// Use this for initialization
+    private bool IsLocked = false;
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        Vector2 objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        transform.position = objectPosition;
+        if (!IsLocked)
+        {
+            Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = objectPosition;
+        }
+        
 		
 	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals(gameObject.name))
+        {
+            transform.position = collision.gameObject.transform.position;
+            IsLocked = true;
+        }
+    }
 }
