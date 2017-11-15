1c1f724c639529dad8e633e8146c85ec3d555d22
yeasinmahi <yeasinmahi72@yahoo.com>
2017-11-15 19:30:39 +0600

    object ditect

--- Assets/Script/drag.cs
+++ Assets/Script/drag.cs
@@ -11,17 +11,41 @@ public class drag : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (!IsLocked)
+
+        if (Input.GetMouseButton(0))
         {
-            Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-            Vector2 objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-            transform.position = objectPosition;
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+                Vector2 objectPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                transform.position = objectPosition;
+            }
         }
+        //if (Input.GetMouseButton(0))
+        //{
+
+        //    Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            
+
+        //    RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
+        //    if (hit.collider != null)
+        //    {
+        //        Vector2 objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        //        transform.position = objectPosition;
+        //    }
+        //}
         
-		
 	}
+
+    public void GetRay()
+    {
+        
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         if (collision.gameObject.name.Equals(gameObject.name))
         {
             transform.position = collision.gameObject.transform.position;
