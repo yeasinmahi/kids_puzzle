cfe1909fbc51058ae15ba701665a03d6a10107b0
yeasinmahi <yeasinmahi72@yahoo.com>
2017-11-15 20:18:26 +0600

    craete game controller

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Drag();
+
+
+    }
+    public void Drag()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.gameObject.tag.Equals("drag"))
+                {
+                    Vector2 objectPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    hit.collider.gameObject.transform.position = objectPosition;
+                }
+
+            }
+        }
+    }
+}
--- Assets/Script/drag.cs
+++ Assets/Script/drag.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class drag : MonoBehaviour {
-
-    private bool IsLocked = false;
+    
 	void Start () {
 		
 	}
@@ -12,22 +11,25 @@ public class drag : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetMouseButton(0))
-        {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        //if (Input.GetMouseButton(0))
+        //{
+        //    RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
 
-            if (hit.collider != null)
-            {
-                Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
-                Vector2 objectPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                transform.position = objectPosition;
-            }
-        }
+        //    if (hit.collider != null)
+        //    {
+        //        if (hit.collider.gameObject.name.Equals(gameObject.name))
+        //        {
+        //            Vector2 objectPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //            transform.position = objectPosition;
+        //        }
+
+        //    }
+        //}
         //if (Input.GetMouseButton(0))
         //{
 
         //    Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-            
+
 
         //    RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
         //    if (hit.collider != null)
@@ -36,20 +38,9 @@ public class drag : MonoBehaviour {
         //        transform.position = objectPosition;
         //    }
         //}
-        
-	}
 
-    public void GetRay()
-    {
-        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-        if (collision.gameObject.name.Equals(gameObject.name))
-        {
-            transform.position = collision.gameObject.transform.position;
-            IsLocked = true;
-        }
     }
 }
