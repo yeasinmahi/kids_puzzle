9256a1a63b456118e7bc56f86236f60428b85c33
yeasinmahi <yeasinmahi72@yahoo.com>
2017-11-16 19:35:38 +0600

    16/11/2017

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -1,13 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public static GameController instance = null;
+    private RaycastHit2D hit;
+    public bool IsLocked = false;
+    void Awake()
+    {
+        if (instance = null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -17,10 +26,13 @@ public class GameController : MonoBehaviour {
     }
     public void Drag()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        }
+
         if (Input.GetMouseButton(0))
         {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-
             if (hit.collider != null)
             {
                 if (hit.collider.gameObject.tag.Equals("drag"))
@@ -30,5 +42,7 @@ public class GameController : MonoBehaviour {
                 }
             }
         }
+        
     }
+    
 }
---
+++ Assets/Script/Reposition.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour {
+
+    private Vector2 currentPosition;
+    
+	void Start () {
+        currentPosition = transform.position;
+	}
+
+	void Update () {
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (!GameController.instance.IsLocked)
+            {
+                transform.position = currentPosition;
+            }
+            
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            GameController.instance.IsLocked = true;
+            transform.position = collision.transform.position;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            GameController.instance.IsLocked = true;
+            transform.position = collider2D.transform.position;
+        }
+    }
+}
